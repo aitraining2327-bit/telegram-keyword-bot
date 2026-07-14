70c6c60c1231b113919ea0ef1c0d80c2e8d81fff
aitraining2327-bit <aitraining2327@gmail.com>
2026-07-14 15:13:53 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-index.js
+const TelegramBot = require("node-telegram-bot-api");
+
+const TOKEN = "توکن_ربات_اینجا";
+const ADMIN_ID = 123456789;
+
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(
+    msg.chat.id,
+    "سلام 👋\nبه ربات خوش آمدید 🌹\n\nکلمه مورد نظر را ارسال کنید."
+  );
+});
+
+
+bot.on("message", (msg) => {
+
+  const text = msg.text;
+
+  if (!text) return;
+
+  if (text === "رقص") {
+    bot.sendMessage(msg.chat.id, "فیلم‌های مربوط به رقص ارسال می‌شود 🎬");
+  }
+
+  else if (text === "عقل") {
+    bot.sendMessage(msg.chat.id, "فیلم‌های مربوط به عقل ارسال می‌شود 🎬");
+  }
+
+});index.js
